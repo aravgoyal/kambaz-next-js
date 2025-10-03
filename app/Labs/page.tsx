b3d028387cfcec893cc366b8e6e6f602b3b31858
Arav Goyal <aravgoyal@outlook.com>
2025-10-03 14:32:47 -0700

    added repo and name

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function Labs() {
  return (
    <div id="wd-labs">
-     <h1>Labs</h1>
+     <h1>Arav Goyal</h1>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
@@ -15,6 +15,10 @@ export default function Labs() {
        <li>
          <Link href="/Labs/Lab3" id="wd-lab3-link">
            Lab 3: JavaScript Fundamentals </Link>
+       </li>
+       <li>
+         <Link href="https://github.com/aravgoyal/kambaz-next-js" id="wd-github">
+           Repository </Link>
        </li>
      </ul>
    </div>
