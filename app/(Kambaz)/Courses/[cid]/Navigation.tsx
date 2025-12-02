a1fc39549418d95a1e3fc33a52cb772e7f69cc41
Arav Goyal <aravgoyal@outlook.com>
2025-12-01 21:26:42 -0800

    any

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -1,27 +1,33 @@
-// app/(Kambaz)/Courses/[cid]/Navigation.tsx
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
-export default function CourseNavigation() {
-  const { cid } = useParams();
+export default function CourseNavigation({ cid }: { cid: string }) {
+  const pathname = usePathname();
+  const links = [
+    { label: "Home", path: "Home" },
+    { label: "Modules", path: "Modules" },
+    { label: "Piazza", path: "Piazza" },
+    { label: "Zoom", path: "Zoom" },
+    { label: "Assignments", path: "Assignments" },
+    { label: "Quizzes", path: "Quizzes" },
+    { label: "Grades", path: "Grades" },
+    { label: "People", path: "People" }
+  ];
   
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link href={`/Courses/${cid}/Home`} id="wd-course-home-link"
-        className="list-group-item active border-0"> Home </Link>
-      <Link href={`/Courses/${cid}/Modules`} id="wd-course-modules-link"
-        className="list-group-item text-danger border-0"> Modules </Link>
-      <Link href={`/Courses/${cid}/Piazza`} id="wd-course-piazza-link"
-        className="list-group-item text-danger border-0"> Piazza </Link>
-      <Link href={`/Courses/${cid}/Zoom`} id="wd-course-zoom-link"
-        className="list-group-item text-danger border-0"> Zoom </Link>
-      <Link href={`/Courses/${cid}/Assignments`} id="wd-course-assignments-link"
-        className="list-group-item text-danger border-0"> Assignments </Link>
-      <Link href={`/Courses/${cid}/Quizzes`} id="wd-course-quizzes-link"
-        className="list-group-item text-danger border-0"> Quizzes </Link>
-      <Link href={`/Courses/${cid}/People/Table`} id="wd-course-people-link"
-        className="list-group-item text-danger border-0"> People </Link>
+      {links.map((link) => (
+        <Link
+          key={link.path}
+          href={`/Courses/${cid}/${link.path}`}
+          className={`list-group-item border-0 ${
+            pathname.includes(link.path) ? "active" : "text-danger"
+          }`}
+        >
+          {link.label}
+        </Link>
+      ))}
     </div>
   );
 }
